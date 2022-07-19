873a9fec98560d0c7e09a1fec2a10b65018bca1a
Berzan <berzan@tutanota.com>
2022-07-19 20:34:26 +0300

    add: tests

--- tests/game.ts
+++ tests/game.ts
@@ -1,16 +1,48 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { expect } from "chai";
 import { Game } from "../target/types/game";
 
 describe("game", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
 
   const program = anchor.workspace.Game as Program<Game>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+
+  it("Can set and change name!", async () => {
+    const [userStatsPDA, _] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode('user-stats'),
+        provider.wallet.publicKey.toBuffer(),
+      ],
+      program.programId
+    )
+
+    await program.methods
+      .createUserStats('Berzan')
+      .accounts({
+        user: provider.wallet.publicKey,
+        userStats: userStatsPDA,
+      })
+      .rpc()
+
+    expect((await program.account.userStats.fetch(userStatsPDA)).name).
+      to.equal('Berzan')
+    
+
+      
+    await program.methods
+      .changeUserName('Zan')
+      .accounts({
+        user: provider.wallet.publicKey,
+        userStats: userStatsPDA,
+      })
+      .rpc()
+
+    expect((await program.account.userStats.fetch(userStatsPDA)).name).
+    to.equal('Zan')
+    
+  })
+})
